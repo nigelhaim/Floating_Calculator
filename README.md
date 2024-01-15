f98b96dc3b2590f95119b5de789b93825ff150c9
nigelhaim <nigelsebastian21@gmail.com>
2024-01-15 14:31:12 +0800

    Completed backend fix TODO: FixFrontend

--- README.md
+++ README.md
@@ -10,3 +10,4 @@ This repository is to attempt to do a floating pop-up calculator in React
 - https://youtu.be/Y20wUiT-8ug?si=hElrkrwh6nw-UUY0
 
 **How to make a react calculator**
+- https://youtu.be/h6lVvsUkdjI?si=3HEElO5NMJfWJ0n7
--- floating-calculator/src/components/Modal/Modal.js
+++ floating-calculator/src/components/Modal/Modal.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import "./Modal.css";
 import Draggable from 'react-draggable';
+import Calculator from '../Calculator/Calculator';
 export default function Modal(){
 
     const [modal, setModal] = useState(false);
@@ -13,22 +14,17 @@ export default function Modal(){
     return (
         <>
     <button onClick={ toggleModal } className='btn-modal'>Open Calculator</button>
-
+<h1> The text and form in the website is to test if you can still navigate in the site while the calculator is still opened</h1>
     {modal && (
     <Draggable>
 
     <div className='modal'>
         <div className='overlay'></div>
         <div className='modal-content'>
-                <h2>Hello Modal</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
+                <h2>Calculator</h2>
                 <button className='close-modal' onClick={toggleModal}>Close</button>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-            </div>
+                <Calculator />
+        </div>
     </div>
 
     </Draggable>
@@ -37,6 +33,12 @@ export default function Modal(){
                 <p>
                     Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                 </p>
+    <div>
+    <form>
+      <input type='text' />
+    </form>
+      <button>Submit</button>
+    </div>
         </>
     );
 }
